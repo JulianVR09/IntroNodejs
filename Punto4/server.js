1497a280001f7aae3253c9953a04bbc7a98a0963
JulianVR09 <julianvillacis.02@gmail.com>
2024-07-12 15:21:31 -0500

    Completed workshop

--- Punto4/server.js
+++ Punto4/server.js
@@ -1,10 +1,10 @@
-const http = require("http");
+const http = require('http');
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello, World!");
-});
+  const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello, World!');
+  });
 
-server.listen(3000, "Esto es un mensaje de prueba", () => {
-  console.log("Server running at http://localhost:3000/");
-});
+  server.listen(3000, 'localhost', () => {
+    console.log('Servidor corriendo en: http://localhost:3000/');
+  });
